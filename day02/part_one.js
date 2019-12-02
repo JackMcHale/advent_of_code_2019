05f8efb70b68ad2df7770052624742180613da0c
JackMcHale <jack.mchale@gmail.com>
2019-12-02 08:52:54 -0500

    Commenting in flow for day two

--- day02/part_one.js
+++ day02/part_one.js
@@ -2,3 +2,16 @@ const file = require('../lib/fileutils.js');
 
 let inputArray = file.getSingleLineIntArayFromFile('day02.input.txt');
 console.log(inputArray.length);
+/*
+To do this, before running the program, replace position 1 with the value 
+12 and replace position 2 with the value 2. What value is left at position 
+0 after the program halts?
+*/
+//Replace the values at 1 and 2
+//Step through the array 4 at a time
+//Operate on line
+//If 1 add
+//If 2 multiple
+//If 99 stop
+
+//Return element 0
