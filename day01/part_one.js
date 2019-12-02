c7af696c5878c97e2b4361c03eebeb3801448ab5
JackMcHale <jack.mchale@gmail.com>
2019-12-01 21:47:17 -0500

    day one, part one

---
+++ day01/part_one.js
@@ -0,0 +1,14 @@
+const file = require('../lib/fileutils.js');
+const fuelReducer = (accumulator,value) =>
+{
+    const numberValue = parseInt(value);
+    let moduleFuel = Math.floor(numberValue/3) - 2;
+    console.log(moduleFuel);
+    return accumulator+ moduleFuel;
+}
+
+
+const values = file.getlinesFromFile('day01.input.txt');
+let answer = values.reduce(fuelReducer,0)
+console.log(answer);
+
